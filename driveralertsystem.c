b102a68b08c22d9abbe3deb39ecc800db738c3bf
jeevams <58686474+jeevams@users.noreply.github.com>
2020-02-15 22:25:23 +0530

    Add files via upload

---
+++ driveralertsystem.c
@@ -0,0 +1,31 @@
+
+#define SENSE A0 
+
+void setup()
+{
+pinMode(SENSE, INPUT);
+pinMode(10, OUTPUT);
+
+
+pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop()
+{
+if(digitalRead(SENSE)) 
+{
+ digitalWrite(LED_BUILTIN, LOW);
+ pinMode(10, LOW);
+}
+else
+{
+  delay (1000);
+  if(digitalRead(SENSE))
+  {
+ digitalWrite(LED_BUILTIN, LOW);
+ pinMode(10, LOW);
+}
+  else
+  digitalWrite(LED_BUILTIN, HIGH);  
+   pinMode(10, HIGH);
+  }
+}
