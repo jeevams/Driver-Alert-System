bc3a91fcc1838b3a86b85554646cb5e51f434e2d
jeevams <58686474+jeevams@users.noreply.github.com>
2020-08-22 22:32:42 +0530

    Update driveralertsystem.c

--- driveralertsystem.c
+++ driveralertsystem.c
@@ -11,21 +11,15 @@ pinMode(LED_BUILTIN, OUTPUT);
 }
 void loop()
 {
-if(digitalRead(SENSE)) 
-{
- digitalWrite(LED_BUILTIN, LOW);
- pinMode(10, LOW);
-}
-else
-{
-  delay (1000);
+
   if(digitalRead(SENSE))
   {
- digitalWrite(LED_BUILTIN, LOW);
- pinMode(10, LOW);
-}
-  else
-  digitalWrite(LED_BUILTIN, HIGH);  
+   digitalWrite(LED_BUILTIN, LOW);
+   pinMode(10, LOW);
+ }
+ else
+ {
+   digitalWrite(LED_BUILTIN, HIGH);  
    pinMode(10, HIGH);
   }
 }
